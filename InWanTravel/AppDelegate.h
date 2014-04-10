d51f549b1f2c7704834c0215794cbd609bc6ffcf
Gavin <wingman639@gmail.com>
2014-04-11 05:55:10 +0800

    Initial Commit

---
+++ InWanTravel/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  InWanTravel
+//
+//  Created by Gavin on 14-4-11.
+//  Copyright (c) 2014年 InWanTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ InWanTravel/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  InWanTravel
+//
+//  Created by Gavin on 14-4-11.
+//  Copyright (c) 2014年 InWanTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
