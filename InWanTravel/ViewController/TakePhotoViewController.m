dc71b010fa35cac1fe918186722cd04c2ee9b542
Gavin <wingman639@gmail.com>
2014-05-26 15:38:32 +0800

    add camera to take passport picture

--- InWanTravel/ViewController/TakePhotoViewController.m
+++ InWanTravel/ViewController/TakePhotoViewController.m
@@ -47,4 +47,43 @@
     [self.navigationController pushViewController:picker animated:NO];
 }
 
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    NSLog(@"didFinishPickingMediaWithInfo");
+    /*
+     
+     [UIApplication sharedApplication].statusBarHidden = NO;
+     
+     NSString *mediaType = [info objectForKey:UIImagePickerControllerMediaType];
+     
+     NSData *data;
+     [picker dismissViewControllerAnimated:YES completion:nil];
+     if ([mediaType isEqualToString:@"public.image"]){
+     
+     //切忌不可直接使用originImage，因为这是没有经过格式化的图片数据，可能会导致选择的图片颠倒或是失真等现象的发生，从UIImagePickerControllerOriginalImage中的Origin可以看出，很原始，哈哈
+     UIImage *originImage = [info objectForKey:UIImagePickerControllerOriginalImage];
+     
+     //图片压缩，因为原图都是很大的，不必要传原图
+     UIImage *scaleImage = [InWanWanImageUtils scaleImage:originImage toScale:0.5];
+     
+     //以下这两步都是比较耗时的操作，最好开一个HUD提示用户，这样体验会好些，不至于阻塞界面
+     if (UIImagePNGRepresentation(scaleImage) == nil) {
+     //将图片转换为JPG格式的二进制数据
+     data = UIImageJPEGRepresentation(scaleImage, 1);
+     } else {
+     //将图片转换为PNG格式的二进制数据
+     data = UIImagePNGRepresentation(scaleImage);
+     }
+     
+     //将二进制数据生成UIImage
+     UIImage *image = [UIImage imageWithData:data];
+     NSString *fileName = [NSString stringWithFormat:@"%@%ld.jpg",[OpenUDID value],[InWanWanDateUtils getNowTimeStamp]];
+     uploadImage = image;
+     uploadName = fileName;
+     [self updatePicInfo:image withFileName:fileName];
+     }
+     [picker dismissViewControllerAnimated:NO completion:nil];
+     */
+}
+
 @end
